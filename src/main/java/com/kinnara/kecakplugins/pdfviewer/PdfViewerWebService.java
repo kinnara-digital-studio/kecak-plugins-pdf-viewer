9e4fcfe70c99072c5f9fe6ebf9d8460e5b250234
Nathanael-Adi <6181901041@student.unpar.ac.id>
2024-09-13 10:49:40 +0700

    revert to only show pdf

--- src/main/java/com/kinnara/kecakplugins/pdfviewer/PdfViewerWebService.java
+++ src/main/java/com/kinnara/kecakplugins/pdfviewer/PdfViewerWebService.java
@@ -14,10 +14,6 @@ import org.joget.commons.util.LogUtil;
 import org.joget.plugin.base.DefaultApplicationPlugin;
 import org.joget.plugin.base.PluginWebSupport;
 
-import org.docx4j.Docx4J;
-import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
-import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
-
 public class PdfViewerWebService extends DefaultApplicationPlugin implements PluginWebSupport{
     final static String LABEL = "PDF Web Service";
 
@@ -57,25 +53,8 @@ public class PdfViewerWebService extends DefaultApplicationPlugin implements Plu
 
         URL url = new URL(urlString);
 
-        if (urlString.endsWith(".docx")) {
-            try (InputStream inputStream = url.openStream()) {
-                // Memuat dokumen DOCX
-                WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(inputStream);
-                
-                LogUtil.info(getClassName(), "Word Package: " + wordMLPackage.getContentType());
-                // Output stream untuk menyimpan hasil PDF
-                MainDocumentPart documentPart = wordMLPackage.getMainDocumentPart();
-                
-                servletResponse.setContentType("application/pdf");
-                // Melakukan konversi DOCX ke PDF
-                Docx4J.toPDF(wordMLPackage, servletResponse.getOutputStream());
-            } catch (Exception e) {
-                LogUtil.error(getClassName(), e, "Error processing DOCX file: " + e.getMessage());
-                servletResponse.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error processing DOCX file");
-            }
-        } else if (urlString.endsWith("xlsx")) {
-            
-        } else {
+        if (urlString.endsWith(".pdf"))
+        {
             try (InputStream inputStream = url.openStream()) {
 
                 servletResponse.setContentType("application/pdf");
@@ -89,6 +68,8 @@ public class PdfViewerWebService extends DefaultApplicationPlugin implements Plu
                     outputStream.write(buffer, 0, bytesRead);
                 }
             }
+        } else {
+            LogUtil.error(getClassName(), null, "File is not a PDF");
         }
     }
 
